b4022f6862cabc0debbd1ef93c698fa4bbd588a3
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-10-30 12:53:40 +0100

    create function operate

--- script.js
+++ script.js
@@ -28,9 +28,25 @@ function divide(a, b) {
 // console.log("Subtraction Test: ", subtract(5, 3));
 // console.log("Multiplication Test: ", multiply(5, 3));
 // console.log("Division Test: ", divide(10, 2));
-// console.log("Division Test with 0: ", divide(10, 20)); 
+// console.log("Division Test with 0: ", divide(10, 0)); 
 
 // Define the variables for the parts of the calculator operation
 let firstNumber = 3;
 let operator = '+';
 let secondNumber = 5;
+
+// Create a new function operate that takes an operator and 2 numbers
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "Invalid operator!";
+    }
+}
